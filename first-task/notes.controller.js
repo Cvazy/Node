56366de8ec0182ad750cab11c5ad5f1bafc34e9d
cvazy <player7712@mail.ru>
2024-06-14 00:18:37 +0300

    First Task

---
+++ first-task/notes.controller.js
@@ -0,0 +1,57 @@
+const fs = require("node:fs/promises");
+const path = require("path");
+const chalk = require("chalk");
+
+const notesPath = path.join(__dirname, "db.json");
+
+async function addNote(title) {
+  const notes = await getNotes();
+
+  const note = {
+    id: Date.now().toString(),
+    title,
+  };
+
+  notes.push(note);
+
+  await fs.writeFile(notesPath, JSON.stringify(notes));
+  console.log(chalk.bgGreen("Note was added!"));
+}
+
+async function removeNote(noteId) {
+  const notes = await getNotes();
+
+  const noteIndex = notes.findIndex(({ id }) => id === noteId);
+
+  notes.splice(noteIndex, 1);
+
+  await fs.writeFile(notesPath, JSON.stringify(notes));
+  console.log(chalk.bgRed(`Note with id=${noteId} was removed!`));
+}
+
+async function getNotes() {
+  const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
+  const notesJSON = JSON.parse(notes);
+
+  return Array.isArray(notesJSON) ? notesJSON : [];
+}
+
+async function printNotes() {
+  const notes = await getNotes();
+
+  if (notes.length) {
+    console.log(chalk.bgBlue("Here is the list of notes:"));
+
+    notes.forEach((el) => {
+      console.log(chalk.blue(`${el.id} ${el.title}`));
+    });
+  } else {
+    console.log(chalk.bgYellow("Notes list is empty"));
+  }
+}
+
+module.exports = {
+  addNote,
+  printNotes,
+  removeNote,
+};
